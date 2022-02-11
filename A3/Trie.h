0436ff12c7035fd193c01de6623acabb71fab1b6
Ethan <u1249938@utah.edu>
2022-02-10 18:24:41 -0700

    Finished with the Trie header

--- A3/Trie.h
+++ A3/Trie.h
@@ -5,10 +5,21 @@
 
 class Trie 
 {
-// in private:
+private:
+    class Node
+    {
+    private:
+        Node* branches[26];
+        bool isWord;
 
+    public:
+        Node(bool initIsWord);
+        bool getIsWord();
+        Node* getBranch(int index);
+        void setBranch(int index, Node* newBranch);
+    };
 
-// in public:
+public:
 
     Trie(); // make this default
     ~Trie();
@@ -17,13 +28,7 @@ class Trie
 
     void addAWord(std::string); // word passed is added to the trie, if already in trie don't add
     bool isAWord(std::string); // if passed an empty string, return false
-    std::vector<std::string> allWordsStartingWithPrefix(std::string) // empty prefix should return all words in trie
-
-
+    std::vector<std::string> allWordsStartingWithPrefix(std::string); // empty prefix should return all words in trie
 };
-
-
-
-
 
 #endif
