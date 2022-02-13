e93a558af26a30637e3c810677b9e301123e4a89
Ethan <u1249938@utah.edu>
2022-02-12 21:21:30 -0700

    The two files passed will be read in and used according to the reqirments

--- A3/TrieTest.cpp
+++ A3/TrieTest.cpp
@@ -1,10 +1,76 @@
-/*
-Specifictations:
-- take in two command line arguments, both a filename
-    - first is a file of words, and second is a file of queries
-    - error checking to see if the files exist
-    - all words from the first file are to be added to the trie
-    - all words in the second file are to be used to test he trie - reported using cout
+#include "Trie.h";
+#include <iostream>
+#include <fstream>
+#include <vector>
 
-- this is the main for this assignment
-*/
+
+void readInTrie(Trie& newTrie, std::string filename)
+{
+    std::ifstream createTrieFile(filename);
+    std::string currentLine;
+    if (createTrieFile.is_open())
+    {
+        while (createTrieFile.good())
+        {
+            createTrieFile >> currentLine;
+            newTrie.addAWord(currentLine);
+        }
+    }
+    else
+    {
+        std::cout << "Could Not Find File";
+    }
+}
+
+void testTrieAgainstWordsInFile(Trie& myTrie, std::string filename)
+{
+    std::ifstream testFile(filename);
+    std::string currentLine;
+    if (testFile.is_open())
+    {
+        while (testFile.good())
+        {
+            testFile >> currentLine;
+            bool isInTrie = myTrie.isAWord(currentLine);
+            if (isInTrie)
+            {
+                std::cout << currentLine << " is found" << std::endl;
+            }
+            else
+            {
+                std::cout << currentLine << " is not found, do you mean:" << std::endl;
+                std::vector<std::string> relatedWords = myTrie.allWordsStartingWithPrefix(currentLine);
+                if (relatedWords.size() == 0)
+                {
+                    std::cout << "   no alternatives found" << std::endl;
+                }
+                else 
+                {
+                    for (int index = 0; index < relatedWords.size(); index++)
+                    {
+                        std::cout << "   " << relatedWords[index] << std::endl;
+                    }
+                }
+            }
+        }
+    }
+    else
+    {
+        std::cout << "Could Not Find File";
+    }
+}
+
+int main(int argc, char **argv)
+{
+    if (argc != 3)
+    {
+        std::cout << "Incorrect Number of Arguments";
+        return 0;
+    }
+
+    Trie newTrie;
+    readInTrie(newTrie, argv[1]);
+    testTrieAgainstWordsInFile(newTrie, argv[2]);
+    return 0;
+}
+
